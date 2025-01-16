60840a6e85d4bb9a6a3b03f4ffcf8221c9f0e609
navnishan <navnishann@gmail.com>
2025-01-16 15:58:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -17,12 +17,18 @@ def create_person_list(people: list) -> list:
         person = Person(name, age)
 
         if 'wife' in person_dict and person_dict['wife']:
-            person.wife = Person.people[person_dict['wife']]
-        elif 'husband' in person_dict and person_dict['husband']:
-            person.husband = Person.people[person_dict['husband']]
-        person_instances.append(person)
-    return person_instances
+            if person_dict['wife'] in Person.people:
+                person.wife = Person.people[person_dict['wife']]
+            else:
+                print(f"Warning: Spouse '{person_dict['wife']}' not found for {person.name}")
 
+        elif 'husband' in person_dict and person_dict['husband']:
+            if person_dict['husband'] in Person.people:
+                person.husband = Person.people[person_dict['husband']]
+            else:
+                print(f"Warning: Spouse '{person_dict['husband']}' not found for {person.name}")
+            person_instances.append(person)
+    return person_instances
 
 
 
