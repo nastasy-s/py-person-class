dac4d27d0bec2db2de47b790c58e5fb63fadec8d
navnishan <navnishann@gmail.com>
2025-01-16 15:36:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.wife = None
+        self.husband = None
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_instances = []
+    for person_dict in people:
+        name = person_dict['name']
+        age = person_dict['age']
+        person = Person(name, age)
+
+        if 'wife' in person_dict and person_dict['wife']:
+            person.wife = Person.people[person_dict['wife']]
+        elif 'husband' in person_dict and person_dict['husband']:
+            person.husband = Person.people[person_dict['husband']]
+        person_instances.append(person)
+    return person_instances
+
+
+
+
+
+
